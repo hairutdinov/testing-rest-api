30aa1c201fdd607c5b6bb9e22f5d9417003fcf55
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-01 11:46:46 +0300

    bug fix: ItemModel() class has 3rd argument

--- tests/integration/models/test_item.py
+++ tests/integration/models/test_item.py
@@ -1,11 +1,13 @@
 from models.item import ItemModel
 from tests.base_test import BaseTest
+from models.store import StoreModel
 
 
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
-            item = ItemModel('test', 100)
+            StoreModel('test').save_to_db()
+            item = ItemModel('test', 100, 1)
             self.assertIsNone(ItemModel.find_by_name('test'))
             item.save_to_db()
             self.assertIsNotNone(ItemModel.find_by_name('test'))
