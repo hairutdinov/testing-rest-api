1e9543b79fb073d0f8f5abb76e0717d7175a3861
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-01 12:49:31 +0300

    creating UnitBaseTest() that imports app

--- tests/unit/models/test_item.py
+++ tests/unit/models/test_item.py
@@ -1,15 +1,16 @@
 from unittest import TestCase
 from models.item import ItemModel
+from tests.unit.unit_base_test import UnitBaseTest
 
 
-class TestItem(TestCase):
+class TestItem(UnitBaseTest):
     def test_create_item(self):
-        item = ItemModel('test', 100)
+        item = ItemModel('test', 100, 1)
         self.assertEqual('test', item.name)
         self.assertEqual(100, item.price)
 
     def test_json(self):
-        item = ItemModel('test', 100)
+        item = ItemModel('test', 100, 1)
         expected = {
             'name': 'test',
             'price': 100,
